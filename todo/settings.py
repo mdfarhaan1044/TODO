02ca52dd5a4f2fce71954b2c89c1da62c5e14bf9
mdfarhaan1044 <mdfarhaan1044@gmail.com>
2025-04-19 14:20:30 +0530

    static files

--- todo/settings.py
+++ todo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,12 +137,13 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-
 import os
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-# Default primary key field type
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/base/static/')
+
+# Optional: enables compressed and versioned static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
