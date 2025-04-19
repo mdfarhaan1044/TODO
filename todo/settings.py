6fb9d50787088091a506b21f1fad12bc7e53abac
mdfarhaan1044 <mdfarhaan1044@gmail.com>
2025-04-19 12:51:01 +0530

    static files fixed

--- todo/settings.py
+++ todo/settings.py
@@ -135,7 +135,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
+
+import os
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 
